b57c9853a60a84e4bbd0c62bd958f4ca6fd2de5a
Dineo Nona <LNDineo@yahoo.com>
2016-09-29 15:09:38 +0200

    Complete functionality of basics

--- app/src/main/java/com/example/codetribe/coffeeorder/MainActivity.java
+++ app/src/main/java/com/example/codetribe/coffeeorder/MainActivity.java
@@ -12,36 +12,78 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+        /**
+         * This method increases the quantity when button plus is clicked
+         */
+                public void increment(View view){
+
+                    quantity = quantity + 1;
+                    displayQuantity(quantity);
+                }
+        /**
+         * This method decreases the quantity when button minus is clicked
+         */
+                public void decrement(View view){
+
+                    quantity = quantity -1;
+                    displayQuantity(quantity);
+                }
+
+        /**This method is called when order button is clicked.*/
+
+                public void submitOrder(View view){
+
+                        int price = 15;
+                        double total = calculatePrice(quantity,price);
+                        displayMessage(createOrderSummary(total));
+
+                }
+
+        /**
+         *This method displays the given quantity value on screen
+         */
+                private void displayQuantity(int numberOfCoffees){
+                    TextView quantityTextView = (TextView) findViewById(R.id.quantity_txt);
+                    quantityTextView.setText(""+ numberOfCoffees);
+                }
+
+                public void displayMessage(String message){
+                    TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+                    orderSummaryTextView.setText(message);
+                    // displayPrice(quantity * 5);
+                }
 
     /**
-     *This method is called when order button is clicked.
+     * This method calculates the price for the coffee orders
      */
-    public void submitOrder(View view){
-        display(2);
-        displayPrice(2 * 5);
-    }
+                private double calculatePrice(int quantity,int pricePerCup){
+                    double total = quantity * pricePerCup;
+                    return  total;
+                }
+
 
     /**
-     *This method displays the given quantity value on screen
+     * Create a summary
      */
-    private void display(int number){
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_txt);
-        quantityTextView.setText(""+ number);
-    }
+                private String createOrderSummary(double total){
+                    String priceMessage = "\nName: Dineo Savage";
+                    priceMessage +=  "\nQuantity: "+ quantity ;
+                    priceMessage += "\nAmount due: R" + total;
+                    priceMessage +="\nThank you!";
+                    return priceMessage;
 
-    /**
-     * This method displays the given quantity value on the screen
-     */
-    @TargetApi(Build.VERSION_CODES.N)
-    private void displayPrice(int number){
+                }
 
-        TextView priceTextView = (TextView)findViewById(R.id.price_txt);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
 
-    }
+
+
+
 }
